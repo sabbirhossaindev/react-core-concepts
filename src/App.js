f1ee3d16780fd3fc569b7e5a72e25006e2e0eb2a
sabbir hossain <mdsabbir477470@gmail.com>
2022-09-19 21:49:26 +0600

    3 color style set dynamic

--- src/App.js
+++ src/App.js
@@ -1,12 +1,30 @@
 import logo from './logo.svg';
 import './App.css';
 
+
+const number = 5555;
+const singer = { name: 'sabbir', job: 'developer' }
+const singer2 = { name: 'nadim', jon: 'student' }
+
+// style liker system 2
+const singerStyle = {
+  color: 'purple',
+  backgroundColor: 'white'
+}
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <h1>JSX</h1>
+        <div className="container">
+          <h3>hello react how are you.</h3>
+        </div>
+        <div className="music">
+          <p>name: { 5555 + number}</p>
+          <p style={singerStyle}>name: {singer.name}{ singer.job}</p>
+          <p style={{color: 'blue', backgroundColor: 'yellow'}}>name: {singer2.name}{ singer2.job}</p>
+        </div>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
