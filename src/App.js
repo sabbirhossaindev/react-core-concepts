c0dfdda477fba27aba1ded4f9d0566023c8a5eb3
sabbir hossain <mdsabbir477470@gmail.com>
2022-09-20 00:39:49 +0600

    3 color style set dynamic

--- src/App.js
+++ src/App.js
@@ -1,42 +1,38 @@
 import logo from './logo.svg';
 import './App.css';
 
-
-const number = 5555;
-const singer = { name: 'sabbir', job: 'developer' }
-const singer2 = { name: 'nadim', job: 'student' }
-
-// style liker system 2
-const singerStyle = {
-  color: 'purple',
-  backgroundColor: 'white'
-}
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1>JSX</h1>
-        <div className="container">
-          <h3>hello react how are you.</h3>
-        </div>
-        <div className="music">
-          <p>name: { 5555 + number}</p>
-          <p style={singerStyle}>name: {singer.name}{ singer.job}</p>
-          <p style={{color: 'blue', backgroundColor: 'yellow'}}>name: {singer2.name}{ singer2.job}</p>
-        </div>
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    </div>
+  );
+}
+
+<div className='App'>
+  <Person name="ruble" nayka="mousumi"></Person>
+  <Person name="bapraz" nayka="popi"></Person>
+  <Person name="sakib" nayka="apu"></Person>
+  <h5>New components, Yey</h5>
+  <Friend movie="shingam" phone="017888"></Friend>
+  <Friend movie="bangla" phone="0192222"></Friend>
+</div>
+
+function Person(props) {
+  console.log(props);
+  return (
+    <div className='person'>
+      <h1>{ props.name}</h1>
+      <p>Nayka: { props.nayka}</p>
+    </div>
+  );
+}
+
+function Friend(props) {
+  // console.log(props)
+  return (
+    <div className='container'>
+      <h1>Name: { props.movie}</h1>
+      <p>phone: { props.phone}</p>
     </div>
   );
 }
